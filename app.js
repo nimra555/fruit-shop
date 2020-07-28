41033339f3f80d44bd50b2d25d5c73400a395b95
Rose Gulab <rosegulab505@gmail.com>
2020-07-28 11:35:47 +0500

    change ui and functionality

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-function userInput(){
-    let inputElement = document.querySelector("input").value;
-    console.log(inputElement);
-    var inputItem = document.querySelector("#input-item");
-    inputItem.innerHTML = "Your Price: " +  inputElement;
-}
-let pApple = 10;
-let pBanana = 12;
-let pMango = 15;
+
+let priceofApple = 10;
+let priceofBanana = 12;
+let priceofMango = 15;
 let totalAmount =  0;
 let quantityOfApple = 0; 
 let quantityOfBanana = 0;
@@ -14,16 +9,29 @@ let quantityOfMango = 0;
 let appleTotalcost = 0;
 let bananaTotalcost = 0;
 let mangoTotalcost = 0;
+let fullQuantity = 0;
 
+function userInput(){
+    let inputElement = document.querySelector("input").value;
+    console.log(inputElement);
+    if(inputElement === ""){
+        alert("Enter your budget");
+    }
+    
+    let inputItem = document.querySelector("#input-item");
+    inputItem.innerHTML = "Your Price: " +  inputElement;
+
+}
 
 function addInputApple(){
    let num = document.querySelector("#num1");
    console.log(num);
-    num.innerHTML  = ++quantityOfApple ;
-    appleTotalcost = quantityOfApple * pApple;
+    num.innerHTML  = ++quantityOfApple;
+    appleTotalcost = quantityOfApple * priceofApple;
     console.log("apple cost " + appleTotalcost);  
-     totalElement(); 
-     balanceAmount();
+    totalElement(); 
+    balanceAmount();
+    totalQuantity();
 }
 
 function SubtractInputApple(){
@@ -31,10 +39,11 @@ function SubtractInputApple(){
     let num = document.querySelector("#num1");
    console.log(num);
     num.innerHTML  = --quantityOfApple ;
-    appleTotalcost= quantityOfApple * pApple;
+    appleTotalcost= quantityOfApple * priceofApple;
     console.log(appleTotalcost);
     totalElement(); 
     balanceAmount();
+    totalQuantity();
     if(quantityOfApple == 0){
         alert("quantity is zero");
     } 
@@ -45,10 +54,11 @@ function addInputBanana(){
     let num2 = document.querySelector("#num2");
     console.log(num2);
      num2.innerHTML  = ++quantityOfBanana ;
-     bananaTotalcost = quantityOfBanana * pBanana;
+     bananaTotalcost = quantityOfBanana * priceofBanana;
      console.log(bananaTotalcost);
      totalElement(); 
      balanceAmount();
+     totalQuantity();
  }
   
 function SubtractInputBanana(){
@@ -56,10 +66,11 @@ function SubtractInputBanana(){
     let num2 = document.querySelector("#num2");
     console.log(num2);
     num2.innerHTML  = --quantityOfBanana ;
-    bananaTotalcost = quantityOfBanana * pBanana;
+    bananaTotalcost = quantityOfBanana * priceofBanana;
     console.log(bananaTotalcost); 
     totalElement(); 
     balanceAmount();
+    totalQuantity();
     if(quantityOfBanana == 0){
          alert("quantity is zero");
     } 
@@ -70,11 +81,11 @@ function SubtractInputBanana(){
     let num3 = document.querySelector("#num3");
     console.log(num3);
      num3.innerHTML  = ++quantityOfMango ;
-     mangoTotalcost = quantityOfMango * pMango;
+     mangoTotalcost = quantityOfMango * priceofMango;
      console.log(mangoTotalcost);
      totalElement();
      balanceAmount(); 
-       
+     totalQuantity();  
  }
 
 
@@ -83,10 +94,11 @@ function SubtractInputBanana(){
      let num3 = document.querySelector("#num3");
     console.log(num3);
      num3.innerHTML  = --quantityOfMango ;
-     mangoTotalcost = quantityOfMango * pMango;
+     mangoTotalcost = quantityOfMango * priceofMango;
      console.log(mangoTotalcost);
      totalElement(); 
      balanceAmount();
+     totalQuantity();
      if(quantityOfMango == 0){
          alert("quantity is zero");
      } 
@@ -104,6 +116,7 @@ function SubtractInputBanana(){
 
    if(totalAmount > inputElement){
         alert("you can't select more than your amount");
+
     }
     else{
         console.log("you can select");
@@ -111,8 +124,16 @@ function SubtractInputBanana(){
 }
 
 function balanceAmount(){
-    let inputElement = document.querySelector("input").value;
+    let inputElement =  document.querySelector("input").value;
    let balance = document.querySelector("#balance");
    balance.innerHTML ="Balance: " + (inputElement - totalAmount);
    console.log(balance);
+}
+
+function totalQuantity(){
+    let  tQuantity = document.querySelector('#total-quantity');
+
+    fullQuantity = (quantityOfApple+quantityOfBanana+quantityOfMango);
+    console.log(fullQuantity);
+    tQuantity.innerHTML = "Quantity of Fruits: " + fullQuantity;
 }
